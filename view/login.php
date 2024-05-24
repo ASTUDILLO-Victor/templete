8917ed610e1230aa69106bf5e525df8edc3176c0
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-05-24 00:23:58 -0500

    ac

--- view/login.php
+++ view/login.php
@@ -35,7 +35,9 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login">
+                                <img src="img/Uventanas.jpg" class="ima" width="500" height="600" alt="agraria">
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
--- view/tables.php
+++ view/tables.php
@@ -1,3 +1,4 @@
+
 <?= require "params/nav.php" ?>
 
 <!-- End of Topbar -->
