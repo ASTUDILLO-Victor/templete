d9ba1cfa11dae4c55d74653ef20fc92c718543c4
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-11 15:07:57 -0500

    prueba

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,18 +1,39 @@
 <?php
+// bootstrap.php
+
+// Verificar la inclusión de App.php
+require_once __DIR__ . '/App.php';
+
+// Autoload function to include class files
+spl_autoload_register(function ($class) {
+    $base_dir = __DIR__ . '/';
+    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
+    
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+// Usar las clases después de cargarlas automáticamente
 use Core\App;
 use Core\Database\QueryBuilder;
 use Core\Database\Connection;
-App::set("config", require '../config.php');
 
-App:: set ('database' , new QueryBuilder(
+// Verificar que la clase App esté cargada correctamente
+if (!class_exists('Core\App')) {
+    die('Class Core\App not found!');
+}
+
+App::set("config", require 'config.php');
+
+App::set('database', new QueryBuilder(
     Connection::start(App::get('config')['database'])
 ));
 
 if (App::get('config')['error_handling']) {
-    //mostrar errores en el navegador 
-    ini_set('display_errors',1);
-    ini_set('display_startup_errors',1);
+    // Mostrar errores en el navegador 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
-
 
