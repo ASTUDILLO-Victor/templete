7ca3169cf7be8b9255550de6a42b2fbfb5bec44d
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-11 15:18:02 -0500

    GUARDA

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -2,17 +2,6 @@
 // bootstrap.php
 
 // Autoload function to include class files
-spl_autoload_register(function ($class) {
-    $base_dir = __DIR__ . '/';
-    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
-    
-    if (file_exists($file)) {
-        require_once $file;
-    } else {
-        die("El archivo para la clase $class no se encuentra en la ruta: $file");
-    }
-});
-
 // Verificar la inclusión de App.php
 require_once __DIR__ . '/app.php';
 
