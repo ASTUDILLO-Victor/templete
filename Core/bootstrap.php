35423000e28b90985d77cd4889f642dc6812d7e9
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-11 15:21:25 -0500

    regrese

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,23 +1,18 @@
 <?php
-// Usar las clases después de cargarlas automáticamente
 use Core\App;
 use Core\Database\QueryBuilder;
 use Core\Database\Connection;
+App::set("config",require 'config.php');
 
-// Verificar que la clase App esté cargada correctamente
-if (!class_exists('Core\App')) {
-    die('Class Core\App not found!');
-}
-
-App::set("config", require 'config.php');
-
-App::set('database', new QueryBuilder(
+App:: set ('database' , new QueryBuilder(
     Connection::start(App::get('config')['database'])
 ));
 
 if (App::get('config')['error_handling']) {
-    // Mostrar errores en el navegador 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
+    //mostrar errores en el navegador 
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
     error_reporting(E_ALL);
 }
+
+
