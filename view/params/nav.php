bb5f24028ce53a81a6b69dccef0442e89bb336dc
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-11 21:09:47 -0500

    cambio del nav

--- view/params/nav.php
+++ view/params/nav.php
@@ -168,9 +168,20 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="index.php?url=reporte">
+                <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapseUtilities2"
+                aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-table"></i>
                     <span>REPORTES</span></a>
+                    <div id="collapseUtilities2" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Concentraciones:</h6>
+                        <a class="collapse-item" href="index.php?url=reporte">Reporte</a>                        
+                        <a class="collapse-item" href="index.php?url=reporte_uno">Partículas Suspendidas en el Aire</a>
+                        <a class="collapse-item" href="index.php?url=reporte2">Compuestos Orgánicos Vólatiles</a>
+
+                    </div>
+                </div>
             </li>
 
         
