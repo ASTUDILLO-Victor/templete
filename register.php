fd2e0c254e8454e1a2dff1363cc873a840855136
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-25 11:59:46 -0500

    add

--- register.php
+++ register.php
@@ -5,9 +5,9 @@ date_default_timezone_set("America/Guayaquil");
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
     $descripcion = "Manualmente"; // Descripción fija para las acciones manuales
-    //$timestamp = date('Y-m-d H:i:s'); // Obtener el timestamp actual
+    $timestamp = date('Y-m-d H:i:s'); // Obtener el timestamp actual
 
-    $sql = "INSERT INTO relay_log (action, descripcion) VALUES ('$action', '$descripcion')";
+    $sql = "INSERT INTO relay_log (action,timestamp, descripcion) VALUES ('$action','$timestamp' ,'$descripcion')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
