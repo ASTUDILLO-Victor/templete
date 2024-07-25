c65ab0ecf6b83c2d9d32d5d87956af8e81edacb0
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-07-25 10:55:58 -0500

    update

---
+++ get_last_log.php
@@ -0,0 +1,16 @@
+<?php
+// get_last_log.php
+include('conexion.php');
+
+$sql = "SELECT * FROM relay_log ORDER BY timestamp DESC LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo json_encode($row);
+} else {
+    echo json_encode([]);
+}
+
+$conn->close();
+?>
