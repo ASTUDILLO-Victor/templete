63902e40ba57291657d3f4d1ec524e0edc23cccd
ASTUDILLO-Victor <167276606+ASTUDILLO-Victor@users.noreply.github.com>
2024-08-02 22:50:08 -0500

    update

--- view/modal/modal.php
+++ view/modal/modal.php
@@ -3,14 +3,15 @@
         <div class="modal-content">
             <div class="modal-header bg-primary text-white">
                 <h2><b>EDITAR REGISTROS</b></h2>
-                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close" onclick="cerrarModal()">
+                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close"
+                    onclick="cerrarModal()">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form id="formix" method="post" action="index.php?url=actualizar">
-                    <input type="hidden" id="modal_id" name="id" class="form-control border-success text-uppercase" onchange="obtenernombredelinput();">
-                    
+                    <input type="hidden" id="modal_id" name="id" class="form-control border-success text-uppercase">
+
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
@@ -19,7 +20,8 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-id-card"></i></span>
                                     </div>
-                                    <input type="text" maxlength="10" minlength="10" onkeypress="return SoloNumeros(event);" class="form-control" id="Ecedu" name="Ecedu" readonly required/>
+                                    <input type="text" maxlength="10" minlength="10" class="form-control" id="Ecedu"
+                                        name="Ecedu" readonly required />
                                 </div>
                             </div>
                             <div class="form-group">
@@ -28,9 +30,8 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-user"></i></span>
                                     </div>
-                                    <input type="text" oninput="validarNombre();" onkeypress="return SoloLetras(event);" class="form-control" id="Enom" name="Enom" required/>
+                                    <input type="text" class="form-control" id="Enom" name="Enom" required />
                                     <div id="nombre-salida" class="text-danger"></div>
-
                                 </div>
                             </div>
                             <div class="form-group">
@@ -39,53 +40,35 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-user"></i></span>
                                     </div>
-                                    <input type="text" oninput="validarApellido();" onkeypress="return SoloLetras(event);" class="form-control" id="Eape" name="Eape" required/>
+                                    <input type="text" class="form-control" id="Eape" name="Eape" required />
                                     <div id="apellido-salida" class="text-danger"></div>
-
                                 </div>
                             </div>
                             <div class="form-group">
-                                    <label for="Email">E-mail:</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
-                                        </div>
-                                        <input type="email" oninput="validarCorreo();" id="Email" name="Email"
-                                            class="form-control" required  readonly/>
+                                <label for="Email">E-mail:</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                                     </div>
-                                    <div id="email-salida" class="text-danger"></div>
+                                    <input type="email" id="Email" name="Email" class="form-control" required
+                                        readonly />
                                 </div>
-                            <!-- <div class="form-group">
+                                <div id="email-salida" class="text-danger"></div>
+                            </div>
+                            <div class="form-group">
                                 <label for="Epo">Rol:</label>
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-user-tag"></i></span>
                                     </div>
-                                    <select id="Epo" class="form-control" name="Epo">
-                                        <option value="1">SuperAdmin</option>
-                                        <option value="2">Admin</option>
-                                        <option value="3">Usuario</option>
+                                    <select id="Epo" class="form-control" name="Epo" required>
+                                        <?php foreach ($todo1 as $user1): ?>
+                                            <option value="<?= htmlspecialchars($user1->id_r) ?>">
+                                                <?= htmlspecialchars($user1->nombre) ?></option>
+                                        <?php endforeach ?>
                                     </select>
                                 </div>
-                            </div> -->
-                            
-                        <!-- Código específico para usuarios -->
-                            <div class="form-group">
-                                <label for="Epo">Rol:</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fas fa-user-tag"></i></span>
-                                    </div>                                
-                                    <select id="Epo" class="form-control" name="Epo" required>
-                                                <?php foreach ($todo1 as $user1):?>
-                                                <option value="<?= htmlspecialchars($user1->id_r) ?>"><?= htmlspecialchars($user1->nombre) ?></option>
-                                                <?php endforeach?>
-                                            </select>
-                                </div>
-
                             </div>
-                    
-                    
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
@@ -102,24 +85,23 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                        <label for="Ecelu">Teléfono:</label>
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                                            </div>
-                                            <input type="text" maxlength="10" minlength="10" oninput="validarTelefono();"
-                                                onkeypress="return SoloNumeros(event);" class="form-control" id="Ecelu"
-                                                name="Ecelu" required   oninput="validarTelefono();"/>
-                                        </div>
-                                        <div id="telefono-salida" class="text-danger"></div>
+                                <label for="Ecelu">Teléfono:</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-phone"></i></span>
                                     </div>
+                                    <input type="text" maxlength="10" minlength="10" class="form-control" id="Ecelu"
+                                        name="Ecelu" required />
+                                </div>
+                                <div id="telefono-salida" class="text-danger"></div>
+                            </div>
                             <div class="form-group">
                                 <label for="Efe">Fecha de Nacimiento:</label>
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
                                     </div>
-                                    <input type="date" class="form-control" id="Efe" name="Efe" required/>
+                                    <input type="date" class="form-control" id="Efe" name="Efe" required />
                                 </div>
                             </div>
                             <div class="form-group">
@@ -128,28 +110,26 @@
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
                                     </div>
-                                    <input type="text" class="form-control" oninput="validarDireccion();" id="Edire" name="Edire" required/>
+                                    <input type="text" class="form-control" id="Edire" name="Edire" required />
                                     <div id="direccion-salida" class="text-danger"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
+
                     <div class="row text-center">
                         <div class="col-md-6">
-                            
                         </div>
                         <div class="col-md-6">
                             <input class="btn btn-success" type="submit" value="Enviar formulario" />
                         </div>
                     </div>
+
                     <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == 2): ?>
-                <!-- Código específico para usuarios -->
-                    <input type="hidden" id="tabla" name="tabla" value="tables3">
+                        <input type="hidden" id="tabla" name="tabla" value="tables3">
                     <?php endif ?>
                     <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == 1): ?>
-                <!-- Código específico para usuarios -->
-                    <input type="hidden" name="tabla" value="tables">
+                        <input type="hidden" name="tabla" value="tables">
                     <?php endif ?>
 
                 </form>
@@ -159,48 +139,48 @@
 </div>
 
 <script>
-$(document).ready(function () {
-    $('#tablita').on('click', 'button[data-action="editar"]', function () {
-        var $row = $(this).closest('tr');
-        var id = $row.find('input[name="id"]').val();
-        var cedula = $row.find('td:eq(0)').text();
-        var nombre = $row.find('td:eq(1)').text();
-        var ape = $row.find('td:eq(2)').text();
-        var email = $row.find('td:eq(3)').text();
-        var Rol = $row.find('td:eq(4)').text();
-        var sex = $row.find('td:eq(5)').text();
-        var celu= $row.find('td:eq(6)').text();
-        var fecha = $row.find('td:eq(7)').text();
-        var dire = $row.find('td:eq(8)').text();
+    $(document).ready(function () {
+        $('#tablita').on('click', 'button[data-action="editar"]', function () {
+            var $row = $(this).closest('tr');
+            var id = $row.find('input[name="id"]').val();
+            var cedula = $row.find('td:eq(0)').text();
+            var nombre = $row.find('td:eq(1)').text();
+            var ape = $row.find('td:eq(2)').text();
+            var email = $row.find('td:eq(3)').text();
+            var Rol = $row.find('td:eq(4)').text();
+            var sex = $row.find('td:eq(5)').text();
+            var celu = $row.find('td:eq(6)').text();
+            var fecha = $row.find('td:eq(7)').text();
+            var dire = $row.find('td:eq(8)').text();
 
-        if (id && cedula &&nombre && ape && email && Rol && sex && celu && fecha && dire ) {
-            $('#modal_id').val(id);
-            $('#Ecedu').val(cedula);
-            $('#Enom').val(nombre);
-            $('#Eape').val(ape);
-            $('#Email').val(email);
-            $('#Epo').val(Rol);
-            $('#Ese').val(sex);
-            $('#Ecelu').val(celu);
-            $('#Efe').val(fecha);
-            $('#Edire').val(dire);
-            
-            $('#mimodal').modal('show');
-        } else {
-            console.error('No se encontraron los valores necesarios para editar.');
-        }
+            if (id && cedula && nombre && ape && email && Rol && sex && celu && fecha && dire) {
+                $('#modal_id').val(id);
+                $('#Ecedu').val(cedula);
+                $('#Enom').val(nombre);
+                $('#Eape').val(ape);
+                $('#Email').val(email);
+                $('#Epo').val(Rol);
+                $('#Ese').val(sex);
+                $('#Ecelu').val(celu);
+                $('#Efe').val(fecha);
+                $('#Edire').val(dire);
+
+                $('#mimodal').modal('show');
+            } else {
+                console.error('No se encontraron los valores necesarios para editar.');
+            }
+        });
     });
-});
 </script>
 <script>
-        //edad 18 años
-        document.addEventListener('DOMContentLoaded', function () {
-            // Calcular la fecha máxima permitida
-            const today = new Date();
-            const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()).toISOString().split('T')[0];
-            document.getElementById('Efe').max = maxDate;
-        });
-        function validarNombre() {
+    //edad 18 años
+    document.addEventListener('DOMContentLoaded', function () {
+        // Calcular la fecha máxima permitida
+        const today = new Date();
+        const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate()).toISOString().split('T')[0];
+        document.getElementById('Efe').max = maxDate;
+    });
+    function validarNombre() {
         var nombre = document.getElementById('Enom').value;
         var elemento = document.getElementById('nombre-salida');
         if (nombre.length < 3) {
@@ -281,10 +261,10 @@ $(document).ready(function () {
             elemento.innerHTML = '';
         }
     }
-    
-    </script>
+
+</script>
 <style>
-.borde {
-    border-radius: 30px;
-}
+    .borde {
+        border-radius: 30px;
+    }
 </style>
